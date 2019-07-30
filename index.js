8bad8c18a5778af5675f4e885b62f40d481e85bb
Max Brooks <Maxbrooks114@gmail.com>
2019-07-30 18:07:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+  const callback = function (driver,i, drivers) {
+    console.log(`${driver.name}`)
+  };
+
+  drivers.forEach(callback);
+}
+
+function logDriversByHometown(drivers, location) {
+  let homies = drivers.filter(driver => driver.hometown === location);
+
+  return homies;
+
+  const callback = function (driver, i, homies) {
+    console.log(`${driver.name}`)
+  };
+    homies.forEach(callback);
+}
